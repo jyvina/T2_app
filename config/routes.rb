f2d73e102aefc301a603a11f7000b20b0644e2dc
kubota masashi <jyvina@gmail.com>
2018-06-18 14:16:58 +0000

    user new create

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,7 @@ Rails.application.routes.draw do
   get 'static_pages/contact'
   get 'static_pages/creater_date'
   get 'users/new'
+  resources :users
   
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
